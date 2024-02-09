3a22c9a8798a1baa308e32f6ae9539cd4fffd5e9
Mikihail <mikihailoy@proton.me>
2024-02-09 16:25:05 +0000

    fix ui bug

--- Code/mLSTMModel.py
+++ Code/mLSTMModel.py
@@ -18,4 +18,28 @@ from reader import *
 from myutils import *
 import logging
 from datetime import datetime
-from multiA
+from multiAttentionRNN import multiAttentionRNN
+from mLSTM import mLSTM
+
+def get_params():
+    parser = argparse.ArgumentParser(description='Short sample app')
+    parser.add_argument('-lstm', action="store", default=150, dest="lstm_units", type=int)
+    parser.add_argument('-epochs', action="store", default=20, dest="epochs", type=int)
+    parser.add_argument('-batch', action="store", default=32, dest="batch_size", type=int)
+    parser.add_argument('-xmaxlen', action="store", default=30, dest="xmaxlen", type=int)
+    parser.add_argument('-ymaxlen', action="store", default=20, dest="ymaxlen", type=int)
+    parser.add_argument('-nopad', action="store", default=False, dest="no_padding", type=bool)
+    parser.add_argument('-lr', action="store", default=0.001, dest="lr", type=float)
+    parser.add_argument('-load', action="store", default=False, dest="load_save", type=bool)
+    parser.add_argument('-verbose', action="store", default=False, dest="verbose", type=bool)
+    parser.add_argument('-l2', action="store", default=0.0003, dest="l2", type=float)
+    parser.add_argument('-dropout', action="store", default=0.1, dest="dropout", type=float)
+    parser.add_argument('-local', action="store", default=False, dest="local", type=bool)
+    opts = parser.parse_args(sys.argv[1:])
+    print "lstm_units", opts.lstm_units
+    print "epochs", opts.epochs
+    print "batch_size", opts.batch_size
+    print "xmaxlen", opts.xmaxlen
+    print "ymaxlen", opts.ymaxlen
+    print "no_padding", opts.no_padding
+    print "regu
